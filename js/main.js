20207ddd85fcde6d6b1c6c9c3cae181154ab4821
Morgan Roberts <morgrob@email.unc.edu>
2021-10-27 23:48:54 -0400

    new pie charts

--- js/main.js
+++ js/main.js
@@ -11,21 +11,7 @@ window.onscroll = function() {
   $lastPosition = $position;
 };
 
-// var chart = c3.generate({
-//   bindto: '#missing-genders',
-//   data: {
-//       columns: [
-//           ['Male', 178747, 95096],
-//           ['Female', 209375, 59369]
-//       ],
-//       type: 'bar'
-//   },
-//   bar: {
-//       width: {
-//           ratio: 0.5 
-//       }
-//   }
-// });
+
 
 var chart = c3.generate({
   bindto: '#missing-demographics',
@@ -55,3 +41,50 @@ var chart = c3.generate({
 }
 });
   
+var chart = c3.generate({
+  bindto: '#offender-sex',
+  data: {
+      columns: [
+          ['Male', 473159],
+          ['Female', 108190],
+          ['Unknown', 36708],
+      ],
+      type : 'pie',
+      onclick: function (d, i) { console.log("onclick", d, i); },
+      onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+      onmouseout: function (d, i) { console.log("onmouseout", d, i); },
+      colors: {
+        'Male': '#FAB4D6',
+        'Female': '#FF8EC6',
+        'Unknown': '#BF5583'
+    },
+    color: function (color, d) {
+        // d will be 'id' when called for legends
+        return d.id && d.id === 'data3' ? d3.rgb(color).darker(d.value / 150) : color;
+    }
+}
+});
+
+var chart = c3.generate({
+  bindto: '#victim-sex',
+  data: {
+      columns: [
+          ['Male', 335813],
+          ['Female', 316885],
+          ['Unknown', 3145],
+      ],
+      type : 'pie',
+      onclick: function (d, i) { console.log("onclick", d, i); },
+      onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+      onmouseout: function (d, i) { console.log("onmouseout", d, i); },
+      colors: {
+        'Male': '#FAB4D6',
+        'Female': '#FF8EC6',
+        'Unknown': '#BF5583'
+    },
+    color: function (color, d) {
+        // d will be 'id' when called for legends
+        return d.id && d.id === 'data3' ? d3.rgb(color).darker(d.value / 150) : color;
+    }
+}
+});
