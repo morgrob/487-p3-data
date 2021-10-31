3b857e72725ffc1432eb22e486c7aa2cecaae266
Morgan Roberts <morgrob@email.unc.edu>
2021-10-31 17:30:18 -0400

    Troubleshooting data table error

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(document).ready( function () {
   $('#exonerations').DataTable( {
     responsive: true,
     ajax: {
-      url: '/data/exonerations.json',
+      url: '../data/exonerations.json',
       dataSrc: ''
     },
     columns: [
@@ -40,7 +40,7 @@ $(document).ready( function () {
     "info":     false,
     "searching": false,
     ajax: {
-      url: '/data/top10.json',
+      url: '../data/top10.json',
       dataSrc: ''
     },
     columns: [
